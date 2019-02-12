c6351ade713aa6e5aac3354907d5ba6940f22614
accforaus <accforaus@gmail.com>
2019-02-12 16:28:20 +0900

    feat: Add Annotation and enum class for link style or data id

---
+++ src/main/kotlin/com/tang/hwplib/annotation/Annotation.kt
@@ -0,0 +1,7 @@
+package com.tang.hwplib.annotation
+
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ID(val type: IDTypes)
+
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LinkID
---
+++ src/main/kotlin/com/tang/hwplib/annotation/IDTypes.kt
@@ -0,0 +1,12 @@
+package com.tang.hwplib.annotation
+
+enum class IDTypes {
+    BinData,
+    TabDef,
+    Numbering,
+    Bullet,
+    BorderFill,
+    CharShape,
+    ParaShape,
+    Style
+}
