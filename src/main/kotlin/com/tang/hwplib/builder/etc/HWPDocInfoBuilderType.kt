69d02ef26802c09635355ebb5a5c612fe251306d
accforaus <accforaus@gmail.com>
2019-02-18 16:24:31 +0900

    feat: Add Document Information Builder Types

---
+++ src/main/kotlin/com/tang/hwplib/builder/etc/HWPDocInfoBuilderType.kt
@@ -0,0 +1,20 @@
+package com.tang.hwplib.builder.etc
+
+enum class HWPDocInfoBuilderType {
+    BinData,
+    BorderFill,
+    Bullet,
+    CharShape,
+    Hangul,
+    English,
+    Hanja,
+    Japanese,
+    Etc,
+    Symbol,
+    User,
+    All,
+    Numbering,
+    ParaShape,
+    Style,
+    TabDef;
+}
